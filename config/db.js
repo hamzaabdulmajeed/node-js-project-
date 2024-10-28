308fe0bb0906a5e40ad94ad0f65942c2eb09b4c8
muhammad hamza <hamzaabdulmajeed863@gmail.com>
2024-10-28 17:10:44 +0500

    backend

--- config/db.js
+++
@@ -1,6 +0,0 @@
-// import mongoose from 'mongoose'
-
-// //connection string
-// mongoose.connect('mongodb+srv://hamzaabdulmajeed863:mhamzaee1@cluster0.xkuy2mb.mongodb.net/')
-
-// export default mongoose
